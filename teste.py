ba5f604becba0c310d509dc4479d616c0c089bca
Juan Juan <juannvictor1998@gmail.com>
2020-03-12 02:11:35 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -43,3 +43,15 @@ df['Estado'] = col
 print(df)
 
 print(df.set_index('Estado'))
+
+
+outside = ['G1', 'G1', 'G1', 'G2', 'G2', 'G2']
+inside = [1,2,3,1,2,3]
+hier_index = list(zip(outside,inside))
+hier_index = pd.MultiIndex.from_tuples(hier_index)
+
+print(hier_index)
+
+df = pd.DataFrame(np.random.randn(6,2), index=hier_index, columns=['A', 'B']) #criando uma tabela de multiníveis
+
+print(df)
